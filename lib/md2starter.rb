2dbbb220761a644bc44d22bdcdb5ace3e652fb08
Atelier Mirai <contact@atelier-mirai.net>
2022-10-06 07:32:00 +0900

    first commit

---
+++ lib/md2starter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "md2starter/version"
+
+module Md2starter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/md2starter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Md2starter
+  VERSION = "0.1.0"
+end
