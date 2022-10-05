9617d9b7b9fba5d5d8846a02f034c6b9d8505fd7
Atelier Mirai <contact@atelier-mirai.net>
2022-10-06 08:06:06 +0900

    improve

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,5 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "md2starter"
 
 require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new(:color => true)]
